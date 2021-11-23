ec157d5793e223affc95dc95b5dcd7081bd7ad3e
devaku <alejokimuy@gmail.com>
2021-11-23 14:11:09 +0800

    testing stuff out

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', async function (req, res) {
+router.get('/', async function (req, res) {
+    console.log(req.session);
+    req.session.mycookie = true;
+    console.log(req.session.id);
+    console.log(req.cookies);
     res.json({
         message: 'Hello World',
+    });
+});
+
+router.get('/logout', (req, res) => {
+    console.log('LOGGING OUT');
+    req.session.destroy();
+
+    console.log(req.session);
+    res.json({
+        message: 'logout',
     });
 });
 
--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const session = require('express-session');
+app.use(
+    session({
+        // Key that will sign the cookie
+        secret: 'dragons',
+
+        // For every request to the server, we want to create a new session, even if we don't care if it's the same user or browser
+        // We don't want this, so it'll be false
+        resave: false,
+
+        // If we have not touched or modified the session, we don't want it to save
+        saveUninitialized: false,
+    })
+);
+
 // For logging purposes on debugging
 const morgan = require('morgan');
 app.use(morgan('short'));
