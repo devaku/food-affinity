3bedae368411e260e8a26e114f4d3a73c1301b38
devaku <alejokimuy@gmail.com>
2021-11-23 14:23:17 +0800

    Working session

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', async function (req, res) {
+router.get('/', async function (req, res) {
+    req.session.mycookies = 'Hello';
     res.json({
         message: 'Hello World',
+    });
+});
+
+router.get('/logout', async function (req, res) {
+    req.session.destroy();
+    res.json({
+        message: 'Logged out',
     });
 });
 
