9240705e8a8aae104d5df812b4d2377879912b11
devaku <alejokimuy@gmail.com>
2021-12-07 10:07:33 +0800

    Added Redux Devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './App';
 const store = createStore(
     reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composeWithDevTools(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
